e9d0796fcdd14185102e64a1feed62b75f3c589f
zahangir710 <zahangir710@gmail.com>
2022-01-20 02:26:00 +0100

    header and nav updated

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function HeaderItem() {
-  return <div></div>;
+export default function HeaderItem({ Icon, title }) {
+  return (
+    <div className="group flex flex-col items-center cursor-pointer w-12 hover:text-white sm:w-20">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+        {title}
+      </p>
+    </div>
+  );
 }
